031e4d86cf2ceeaa47eb3090f10786c3203ec4e3
solate <349370626@qq.com>
2025-12-10 16:57:31 +0800

    feat(common): 添加通用结构体和分页功能
    添加 TimeRange 和 IDRequest 结构体用于通用请求参数
    实现 PageRequest 和 PageResponse 分页相关结构体及方法

---
+++ backend/pkg/common/common.go
@@ -0,0 +1,11 @@
+package common
+
+type TimeRange struct {
+	StartTime string `form:"start_time"` // 开始时间
+	EndTime   string `form:"end_time"`   // 结束时间
+}
+
+// IDRequest ID请求
+type IDRequest struct {
+	ID uint `uri:"id" json:"id" binding:"required,min=1"`
+}
